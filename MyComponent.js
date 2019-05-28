5d9195931eb053ffc62c7d4035a66a05b958e5af
opexa <nikokat@abv.bg>
2019-05-28 11:03:45 +0300

    The initial commit

---
+++ MyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyComponent = () => (
+  <div className='my-component'>
+    <h2>Hello,</h2>
+    <h3>I am Nikola Katrandzhiev.</h3>
+
+    <p>I am an 18 years old boy from Varna, Bulgaria. <br/> Since I was fifteen I'm exploring new horizonts in the world of web development.</p>
+    <p>To me the Goodbag project looks like something I would love to work on, because I like innovative and entrepreneurial projects and your bag looks like one for sure. <br/> Technical wise I use ReactJS in many of my own projects. I've been using both Flux and Redux in past and current projects.</p>
+
+    <p><i>Thank you for your time.</i></p> 
+    <p><i> Kind Regards, <br/>Nikola</i></p>
+  </div>
+);
+
+export default MyComponent;
